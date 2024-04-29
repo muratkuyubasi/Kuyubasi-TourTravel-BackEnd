bb6decf8df87bf994434bab72af348db8781bee7
Murat Kuyubaşı <96579859+muratkuyubasi@users.noreply.github.com>
2024-04-29 23:45:04 +0300

    Lets gooo !!!!!

---
+++ TourV2.MediatR/Handlers/Tour/Reservation/GetAllTourReservationQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using TourV2.Data.Dto;
+using TourV2.MediatR.Queries;
+using TourV2.Repository;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace TourV2.MediatR.Handlers
+{
+    public class GetAllTourReservationQueryHandler : IRequestHandler<GetAllTourReservationQuery, List<TourReservationDto>>
+    {
+        private readonly ITourReservationRepository _tourReservationRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllTourReservationQueryHandler(
+            ITourReservationRepository tourReservationRepository,
+            IMapper mapper)
+        {
+            _tourReservationRepository = tourReservationRepository;
+            _mapper = mapper;
+
+        }
+        public async Task<List<TourReservationDto>> Handle(GetAllTourReservationQuery request, CancellationToken cancellationToken)
+        {
+            var entities = await _tourReservationRepository.AllIncluding(
+                at=>at.ActiveTour.TourRecord, p => p.ActiveTour.PeriodRecord).Where(x=>!x.IsDeleted).ToListAsync();
+            return _mapper.Map<List<TourReservationDto>>(entities);
+        }
+    }
+}
